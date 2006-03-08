dc6b74ae625e8764434ccb957c0b969fc4d320ac
eu <eu@c3de99e0-e006-0410-afd8-f735d099a084>
2006-03-08 06:16:22 +0000

    MNGECLIPSE-84 Added launch shortcut and keyboard bindings

--- org.maven.ide.eclipse/src/org/maven/ide/eclipse/actions/ExecutePomAction.java
+++ org.maven.ide.eclipse/src/org/maven/ide/eclipse/actions/ExecutePomAction.java
@@ -9,6 +9,7 @@ import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.core.runtime.Platform;
 import org.eclipse.debug.core.DebugPlugin;
+import org.eclipse.debug.core.ILaunch;
 import org.eclipse.debug.core.ILaunchConfiguration;
 import org.eclipse.debug.core.ILaunchConfigurationType;
 import org.eclipse.debug.core.ILaunchConfigurationWorkingCopy;
@@ -33,6 +34,9 @@ import org.maven.ide.eclipse.util.Tracer;
 public class ExecutePomAction implements ILaunchShortcut, Maven2LaunchConstants, ITraceable {
   private static final boolean TRACE_ENABLED = Boolean.valueOf(Platform.getDebugOption("org.maven.ide.eclipse/actions")).booleanValue();
 
+  private boolean showDialog = false;
+
+  
   public boolean isTraceEnabled() {
     return TRACE_ENABLED;
   }
@@ -41,7 +45,7 @@ public class ExecutePomAction implements ILaunchShortcut, Maven2LaunchConstants,
     if (!(selection instanceof IStructuredSelection)) {
       return;
     }
-    IStructuredSelection structuredSelection = (IStructuredSelection)selection;
+    IStructuredSelection structuredSelection = (IStructuredSelection) selection;
     
     IResource resource = null;
 
@@ -49,7 +53,7 @@ public class ExecutePomAction implements ILaunchShortcut, Maven2LaunchConstants,
     if (object instanceof IResource) {
       resource = ( IResource ) object;
     } else if (object instanceof IAdaptable) {
-      resource = (IResource)((IAdaptable)object).getAdapter(IResource.class);
+      resource = (IResource)((IAdaptable) object).getAdapter(IResource.class);
     }
     if( resource==null) {
       return;
@@ -65,24 +69,23 @@ public class ExecutePomAction implements ILaunchShortcut, Maven2LaunchConstants,
       return;
     }
     
-    boolean openDialog = true;
-    try {
-      // if no goals specified
-      String goals = launchConfiguration.getAttribute(ATTR_GOALS, (String)null);
-      openDialog = (goals == null || goals.trim().length() == 0);
-    }
-    catch (CoreException e) {
-      Tracer.trace(this, "Error creating new launch configuration", "", e);
-      Maven2Plugin.log(e);
+    if (!showDialog){
+      try {
+        // if no goals specified
+        String goals = launchConfiguration.getAttribute(ATTR_GOALS, (String)null);
+        showDialog = (goals == null || goals.trim().length() == 0);
+      } catch (CoreException e) {
+        Tracer.trace(this, "Error creating new launch configuration", "", e);
+        Maven2Plugin.log(e);
+      }
     }
     
-    if (openDialog) {
+    if (showDialog) {
       Tracer.trace(this, "Opening dialog for launch configuration", launchConfiguration.getName());
       DebugUITools.saveBeforeLaunch();
       DebugUITools.openLaunchConfigurationDialog(Maven2Plugin.getDefault().getWorkbench().getActiveWorkbenchWindow().getShell(), 
           launchConfiguration, Maven2LaunchMainTab.ID_EXTERNAL_TOOLS_LAUNCH_GROUP, null);
-    }
-    else {
+    } else {
       Tracer.trace(this, "Launching configuration", launchConfiguration.getName());
       DebugUITools.launch(launchConfiguration, ILaunchManager.RUN_MODE);
     }
@@ -94,28 +97,43 @@ public class ExecutePomAction implements ILaunchShortcut, Maven2LaunchConstants,
     ILaunchConfigurationType launchConfigurationType = launchManager.getLaunchConfigurationType(LAUNCH_CONFIGURATION_TYPE_ID);
 
     // scan existing launch configurations
-    try {
-      ILaunchConfiguration[] launchConfigurations = launchManager.getLaunchConfigurations(launchConfigurationType);
-      for( int i = 0; i < launchConfigurations.length; i++ ) {
-        ILaunchConfiguration cfg = launchConfigurations[i];
-        Tracer.trace(this, "Processing existing launch configuration", cfg.getName());
-        // don't forget to substitute variables
-        String workDir = Maven2Plugin.substituteVar(cfg.getAttribute(ATTR_POM_DIR, (String)null));
-        if (workDir == null) {
-          Tracer.trace(this, "Launch configuration doesn't have workdir!");
-          continue;
+    if (!showDialog){
+      try {
+        ILaunch[] launches = launchManager.getLaunches();
+        ILaunchConfiguration[] launchConfigurations = null;
+        
+        if (launches.length > 0){
+          for (int i=0;i<launches.length;i++){
+            if (launches[i].getLaunchConfiguration().getType().equals(launchConfigurationType)){
+              launchConfigurations = new ILaunchConfiguration[]{launches[i].getLaunchConfiguration()};
+            }
+          }
         }
-        Tracer.trace(this, "Workdir", workDir);
-        IPath workPath = new Path(workDir);
-        if (basedir.equals(workPath)) {
-          Tracer.trace(this, "Found matching existing configuration", cfg.getName());
-          return cfg;
+        if (launchConfigurations == null){
+          launchConfigurations = launchManager.getLaunchConfigurations(launchConfigurationType);
         }
+        for( int i = 0; i < launchConfigurations.length; i++ ) {
+          ILaunchConfiguration cfg = launchConfigurations[i];
+          Tracer.trace(this, "Processing existing launch configuration", cfg.getName());
+          // don't forget to substitute variables
+          String workDir = Maven2Plugin.substituteVar(cfg.getAttribute(ATTR_POM_DIR, (String)null));
+          if (workDir == null) {
+            Tracer.trace(this, "Launch configuration doesn't have workdir!");
+            continue;
+          }
+          Tracer.trace(this, "Workdir", workDir);
+          IPath workPath = new Path(workDir);
+          if (basedir.equals(workPath)) {
+            Tracer.trace(this, "Found matching existing configuration", cfg.getName());
+            return cfg;
+          }
+        }
+      } catch (CoreException e) {
+        Tracer.trace(this, "Error scanning existing launch configurations", "", e);
+        Maven2Plugin.log(e);
       }
-    }
-    catch (CoreException e) {
-      Tracer.trace(this, "Error scanning existing launch configurations", "", e);
-      Maven2Plugin.log(e);
+  
+      Tracer.trace(this, "No existing configurations found, creating new");
     }
 
     Tracer.trace(this, "No existing configurations found, creating new");
@@ -127,16 +145,16 @@ public class ExecutePomAction implements ILaunchShortcut, Maven2LaunchConstants,
       workingCopy.setAttribute(ATTR_POM_DIR, basedir.toString());
       
       // set other defaults if needed
-      //Maven2LaunchMainTab maintab = new Maven2LaunchMainTab();
-      //maintab.setDefaults(workingCopy);
+      // Maven2LaunchMainTab maintab = new Maven2LaunchMainTab();
+      // maintab.setDefaults(workingCopy);
       // maintab.dispose();
       
       ILaunchConfiguration newConfiguration = workingCopy.doSave();
       return newConfiguration;
-    }
-    catch (CoreException e) {
-      Tracer.trace(this, "Error creating new launch configuration", newName, e);
-      Maven2Plugin.log(e);
+    } catch (Exception e) {
+      String msg = "Error creating new launch configuration";
+      Tracer.trace(this, msg, newName, e);
+      Maven2Plugin.log(msg, e);
     }
     return null;
   }
@@ -144,8 +162,10 @@ public class ExecutePomAction implements ILaunchShortcut, Maven2LaunchConstants,
   public void launch( IEditorPart editor, String mode ) {
     // nothing for now
   }
-  
-  
+
+  protected void setShowDialog( boolean showDialog) {
+    this.showDialog = showDialog;
+  }
   
 }
 
